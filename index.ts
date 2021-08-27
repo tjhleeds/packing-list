92c1f639a84dc240efd392145e0a4b1ab3fe4222
Tim Hilton <tim.hilton@audacia.co.uk>
2021-08-27 12:51:39 +0100

    Remove old code

--- index.ts
+++ index.ts
@@ -5,30 +5,8 @@ const runApp = () => {
     console.log('App running!');
     setTimeout(() => {
         const appDiv = document.getElementById('app');
-        appDiv.innerHTML = `<h1>TypeScript Starter with changed ts which hasn't been recompiled locally</h1>`;
+        appDiv.innerHTML = `<h1>Packing list</h1>`;
     }, 1000);
 }
 
 runApp();
-
-// // document.addEventListener("DOMContentLoaded", runApp);
-// console.log('starting');
-// if(window.attachEvent) {
-//     console.log('attachEvent found');
-//     window.attachEvent('onload', runApp);
-// } else {
-//     if(window.onload) {
-//         console.log('onload found');
-//         const curronload = window.onload;
-//         var newonload = function(evt: Event) {
-//             curronload(evt);
-//             runApp();
-//         };
-//         window.onload = newonload;
-//     } else {
-//         console.log('setting onload');
-//         window.onload = runApp;
-//     }
-// }
-
-// trivial change to test deployment trigger on netlify - now with correct branch name!
