2bd3f58c7ef8240c72a2e7aaf988e4ec83bd2453
Tim Hilton <tim.hilton@audacia.co.uk>
2021-08-27 12:00:53 +0100

    Code running locally

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // Import stylesheets
 // import './style.css';
-
 const runApp = () => {
     console.log('App running!');
     setTimeout(() => {
         const appDiv = document.getElementById('app');
         appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
     }, 1000);
-}
-
+};
 document.addEventListener("DOMContentLoaded", runApp);
