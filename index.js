cae3bb5145b96dc3bf45383c8582f20222d4edc1
Tim Hilton <tim.hilton@audacia.co.uk>
2021-08-27 12:46:37 +0100

    Compiled ts

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ const runApp = () => {
         appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
     }, 1000);
 };
-// document.addEventListener("DOMContentLoaded", runApp);
-if (window.attachEvent) {
-    window.attachEvent('onload', runApp);
-}
-else {
-    if (window.onload) {
-        const curronload = window.onload;
-        var newonload = function (evt) {
-            curronload(evt);
-            runApp();
-        };
-        window.onload = newonload;
-    }
-    else {
-        window.onload = runApp;
-    }
-}
+runApp();
+// // document.addEventListener("DOMContentLoaded", runApp);
+// console.log('starting');
+// if(window.attachEvent) {
+//     console.log('attachEvent found');
+//     window.attachEvent('onload', runApp);
+// } else {
+//     if(window.onload) {
+//         console.log('onload found');
+//         const curronload = window.onload;
+//         var newonload = function(evt: Event) {
+//             curronload(evt);
+//             runApp();
+//         };
+//         window.onload = newonload;
+//     } else {
+//         console.log('setting onload');
+//         window.onload = runApp;
+//     }
+// }
+// trivial change to test deployment trigger on netlify - now with correct branch name!
